a4401baf58d763125a0f8a19fd0697b5dd98eebe
OVERockq <locustk@gmail.com>
2024-02-16 19:54:20 +0900

    Update Data-SearchHans.py
    * 검색 기능 보완
    * UI 변경
    * 좌표 가저오기 기능 추가

--- Data-SearchHans.py
+++ Data-SearchHans.py
@@ -3,52 +3,61 @@ import csv
 import shutil
 import tkinter as tk
 from tkinter import filedialog, ttk
+import subprocess
+from xml.etree import ElementTree as ET
 
 class CustomExplorer:
     def __init__(self, root):
         self.root = root
         self.root.title("Data SeachHans v0.0.1")
-        self.root.geometry("1200x800")
+        self.root.geometry("1320x800")
 
         self.selected_folder = ""
-        self.table_columns = ["DataName"] + ["_{}".format(i) for i in range(1, 21)]
-        self.column_widths = {"DataName": 200, "_1": 100, "_2": 100, "_3": 100}  # Adjust column widths as needed
+        self.table_columns = ["DataName", "Source", "DateTime", "RequestNo", "SceneNo", "Direction", "Swath", "Mode", "P.Type1", "P.Type2", "Level", "Lat", "Lon"]  # Modified column names
+        self.column_widths = {"DataName": 380, "Source": 60, "DateTime": 120, "Direction": 60, "Mode": 60, "Level": 60, "P.Type1": 60, "P.Type2": 60, "Lat": 100, "Lon": 100}  # Adjust column widths as needed
 
         # First Row (Button Row)
         self.button_row = tk.Frame(root)
-        self.button_row.pack(side=tk.TOP, fill=tk.X)
+        self.button_row.pack(side=tk.TOP, fill=tk.X, padx=(0, 10))
 
-        self.select_folder_button = tk.Button(self.button_row, text="Select Folder", command=self.select_folder)
+        self.select_folder_button = tk.Button(self.button_row, text="폴더 선택", command=self.select_folder)
         self.select_folder_button.pack(side=tk.LEFT, padx=10)
 
-        self.selected_path_label = tk.Label(self.button_row, text="")
-        self.selected_path_label.pack(side=tk.LEFT)
+        self.export_csv_button = tk.Button(self.button_row, text="CSV 내보내기", command=self.export_to_csv)
+        self.export_csv_button.pack(side=tk.LEFT, padx=10)
 
-        self.export_csv_button = tk.Button(self.button_row, text="Export to CSV", command=self.export_to_csv)
-        self.export_csv_button.pack(side=tk.RIGHT, padx=10)
+        self.copy_to_path_button = tk.Button(self.button_row, text="폴더 복사", command=self.copy_to_path)
+        self.copy_to_path_button.pack(side=tk.LEFT, padx=10)
 
-        self.copy_to_path_button = tk.Button(self.button_row, text="Copy to Path", command=self.copy_to_path)
-        self.copy_to_path_button.pack(side=tk.RIGHT)
+        self.filter_entry_label = tk.Label(self.button_row, justify="right", text="▶검색(띄워쓰기 AND 조건) :")
+        self.filter_entry_label.pack(side=tk.LEFT, padx=5)
 
         self.filter_entry = tk.Entry(self.button_row, justify="left", width=30)
-        self.filter_entry.pack(side=tk.RIGHT, padx=(20, 10), fill=tk.X)
-        
-        self.filter_entry_label = tk.Label(self.button_row, text="Search")
-        self.filter_entry_label.pack(side=tk.RIGHT)
+        self.filter_entry.pack(side=tk.LEFT, padx=(0, 5), fill=tk.X)
+
+        # Bind button to update table
+        self.update_table_button = tk.Button(self.button_row, text="검색", command=self.update_table)
+        self.update_table_button.pack(side=tk.LEFT, padx=5)
+
+        self.filter_cancel_button = tk.Button(self.button_row, text="초기화", command=self.cancel_filter)
+        self.filter_cancel_button.pack(side=tk.LEFT, padx=2)
 
         # Second Row (Table Row)
         self.table_frame = tk.Frame(root)
-        self.table_frame.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+        self.table_frame.pack(side=tk.TOP, fill=tk.BOTH, padx=(10, 10), expand=True)
 
         self.table = ttk.Treeview(self.table_frame, columns=self.table_columns, show="headings", height=20)
         for column in self.table_columns:
             self.table.heading(column, text=column, command=lambda c=column: self.sort_column(c, False))
-            self.table.column(column, width=self.column_widths.get(column, 100), anchor="center")
+            self.table.column(column, width=self.column_widths.get(column, 75), anchor="center")
         self.table.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
 
         table_scroll_y = tk.Scrollbar(self.table_frame, orient=tk.VERTICAL, command=self.table.yview)
         table_scroll_y.pack(side=tk.RIGHT, fill=tk.Y)
         self.table.configure(yscrollcommand=table_scroll_y.set)
+
+        # Double click event handler for table items
+        self.table.bind("<Double-1>", self.double_click_handler)
 
         # Third Row (Status Bar Row)
         self.status_bar_frame = tk.Frame(root)
@@ -58,28 +67,31 @@ class CustomExplorer:
         self.status_bar = tk.Label(self.status_bar_frame, textvariable=self.status_var, bd=1, relief=tk.SUNKEN, anchor=tk.W)
         self.status_bar.pack(side=tk.BOTTOM, fill=tk.X)
 
-        self.update_selected_path()
-        self.update_table()
+        # "L1C 데이터에서 위치 가져오기" 버튼
+        self.get_coordinates_button = tk.Button(self.status_bar_frame, text="동일 L1C 데이터에서 위치불러오기", command=self.get_coordinates)
+        self.get_coordinates_button.pack(side=tk.BOTTOM, padx=10, pady=5)
 
-        # Bind entry to update table in real-time
-        self.filter_entry.bind("<KeyRelease>", self.update_table)
+        self.update_selected_path()
 
     def select_folder(self):
         self.selected_folder = filedialog.askdirectory()
         self.update_selected_path()
-        self.update_table()
+        self.update_table()  # Update table immediately after selecting folder
 
     def update_selected_path(self):
-        self.selected_path_label.config(text=f"{self.selected_folder}")
+        num_folders = 0
+        if os.path.isdir(self.selected_folder):
+            num_folders = len([name for name in os.listdir(self.selected_folder) if os.path.isdir(os.path.join(self.selected_folder, name))])
+        self.root.title(f"Data SeachHans v0.0.1 - {self.selected_folder} ({num_folders} datasets)")  # Update window title with selected folder and number of datasets
 
-    def update_table(self, event=None):
+    def update_table(self):
         filter_text = self.filter_entry.get().lower()
         self.table.delete(*self.table.get_children())
         if os.path.isdir(self.selected_folder):
             for folder_name in os.listdir(self.selected_folder):
                 folder_path = os.path.join(self.selected_folder, folder_name)
                 if os.path.isdir(folder_path) and all(term.lower() in folder_name.lower() for term in filter_text.split(" ")):
-                    folder_data = [folder_name] + folder_name.split("_")
+                    folder_data = [folder_name] + folder_name.split("_")[:10]  # Modify folder_data
                     self.table.insert("", "end", values=folder_data)
 
     def sort_column(self, col, reverse):
@@ -92,29 +104,68 @@ class CustomExplorer:
             self.table.move(item[1], "", i)
         self.table.heading(col, command=lambda: self.sort_column(col, not reverse))
 
+    def cancel_filter(self):
+        self.filter_entry.delete(0, tk.END)
+        self.update_table()
+
+    def double_click_handler(self, event):
+        selected_item = self.table.selection()[0]
+        folder_name = self.table.item(selected_item, "values")[0]
+        click_path = os.path.join(self.selected_folder, folder_name).replace('/', '\\')  # Replace forward slashes with backward slashes
+        print(click_path)
+        subprocess.Popen(f'explorer "{click_path}"')  # Open folder in file explorer
+
     def export_to_csv(self):
-        file_path = filedialog.asksaveasfilename(defaultextension=".csv", filetypes=[("CSV Files", "*.csv")])
-        if file_path:
-            with open(file_path, "w", newline="") as csvfile:
-                csv_writer = csv.writer(csvfile)
-                csv_writer.writerow(self.table_columns)
-                for child in self.table.get_children():
-                    values = self.table.item(child)["values"]
-                    csv_writer.writerow(values)
-            self.status_var.set("CSV exported successfully.")
+        filename = filedialog.asksaveasfilename(defaultextension=".csv", filetypes=[("CSV files", "*.csv")])
+        if filename:
+            with open(filename, mode="w", newline="") as file:
+                writer = csv.writer(file)
+                writer.writerow(self.table_columns)
+                for item in self.table.get_children():
+                    writer.writerow([self.table.item(item, "values")[i] for i in range(len(self.table_columns))])
 
     def copy_to_path(self):
-        destination_path = filedialog.askdirectory()
-        if os.path.isdir(destination_path):
-            for child in self.table.selection():
-                folder_name = self.table.item(child)["values"][0]
+        destination_folder = filedialog.askdirectory()
+        if destination_folder:
+            for item in self.table.selection():
+                folder_name = self.table.item(item, "values")[0]
                 source_path = os.path.join(self.selected_folder, folder_name)
-                destination_folder = os.path.join(destination_path, folder_name)
-                shutil.copytree(source_path, destination_folder)
-            self.status_var.set("Folders copied successfully.")
-        else:
-            self.status_var.set("Invalid destination path.")
+                shutil.copytree(source_path, os.path.join(destination_folder, folder_name))
 
+    def get_coordinates(self):
+        for child in self.table.get_children():
+            folder_name = self.table.item(child)["values"][0]
+            folder_path = os.path.join(self.selected_folder, folder_name)
+            if len(folder_name) >= 50 and folder_name[49] == 'C':  # Validate folder name
+                kml_file = self.find_kml_file(folder_path)
+                if kml_file:
+                    Lat, Lon = self.get_kml_center(kml_file)
+                    self.table.set(child, "Lat", Lat)
+                    self.table.set(child, "Lon", Lon)
+
+    def find_kml_file(self, folder_path):
+        for file_name in os.listdir(folder_path):
+            if file_name.lower().endswith('.kml'):
+                return os.path.join(folder_path, file_name)
+        return None
+
+    def get_kml_center(self, kml_file):
+        tree = ET.parse(kml_file)
+        root = tree.getroot()
+
+        # Find Polygon coordinates from gx:LatLonQuad
+        coordinates_str = root.find(".//{http://www.opengis.net/kml/2.2}coordinates").text.strip()
+        coordinates_list = [coord.strip().split(",") for coord in coordinates_str.split()]
+
+        # Calculate polygon center coordinates
+        if coordinates_list:
+            Lats = [float(coord[1]) for coord in coordinates_list]
+            Lons = [float(coord[0]) for coord in coordinates_list]
+            center_Lat = sum(Lats) / len(Lats)
+            center_Lon = sum(Lons) / len(Lons)
+            return center_Lat, center_Lon
+        else:
+            return None, None
 
 if __name__ == "__main__":
     root = tk.Tk()
